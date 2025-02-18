78e589d913166aa19ffba944cd3efb25bfd65a08
Sudip Ghosh <sudip02ghosh@gmail.com>
2025-02-19 02:08:27 +0530

    cros origin

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
   })
 );
